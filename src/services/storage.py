07c26bc496d29e2942b77624cab29ed7db3f66eb
KlimukhMaksim <125072343+KlimukhMaksim@users.noreply.github.com>
2026-07-13 09:15:10 +0300

    Fix/cl 132 adapt bird watching code to work on ec2 (#8)
    * fix(storage-service):CL-132 Added S3 region and commented S3 keys
    
    * fix(app):CL-132 Change app port

--- src/services/storage.py
+++ src/services/storage.py
@@ -15,9 +15,10 @@ def get_s3_client():
     """Initialize and return a configured boto3 S3 client connection."""
     return boto3.client(
         "s3",
-        endpoint_url=os.getenv("S3_ENDPOINT"),
-        aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
-        aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
+        region_name=os.getenv("S3_REGION"),
+        # endpoint_url=os.getenv("S3_ENDPOINT"),
+        # aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
+        # aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
         config=Config(s3={"addressing_style": "path"}, signature_version="s3v4"),
         use_ssl=False,
     )
